ef7522218b749189ebc90786ccfb87adc12933a3
md muzammal huqe <muzammalraj@gmail.com>
2024-01-26 19:50:31 +0600

    email varification on render link updated

--- accounts/views.py
+++ accounts/views.py
@@ -34,7 +34,7 @@ class UserRegistrationView(CreateView):
         user.save()
         token = default_token_generator.make_token(user)
         uid = urlsafe_base64_encode(force_bytes(user.pk))
-        confirm_link = f"http://127.0.0.1:8000/accounts/active/{uid}/{token}"
+        confirm_link = f"https://tutormediabd.onrender.com/accounts/active/{uid}/{token}"
         email_subject = "Confirm Your Email"
         email_body = render_to_string('confirm_email.html', {'confirm_link' : confirm_link})
             
